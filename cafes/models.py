a943316718afb5c9a8d18122f1308537e34e3f1f
Abdul Halimi <abdulhalimi13@yahoo.com>
2019-10-17 21:45:59 -0400

    Adding column to postgres

--- cafes/models.py
+++ cafes/models.py
@@ -6,5 +6,7 @@ class Cafe(models.Model):
     image = models.ImageField(upload_to='images/')
     #Summary
     summary = models.CharField(max_length=200)
+    #Name
+    name = models.CharField(max_length=50)
     def __str__(self):
         return self.summary
