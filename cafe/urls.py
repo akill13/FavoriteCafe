26603844cc7ba0f3b7f38397e9d5d7e0cd257b3f
Abdul Halimi <abdulhalimi13@yahoo.com>
2019-10-17 21:20:33 -0400

    Added URL routing based off cafe ID

--- cafe/urls.py
+++ cafe/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', cafes.views.homepage, name='home'),
-    path('cafes/<str:cafe_name>', cafes.views.detail, name='detail')
+    path('cafe/<int:cafe_id>', cafes.views.detail, name='detail')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cafes/views.py
+++ cafes/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cafe
 # Create your views here.
 def homepage(request):
     cafes = Cafe.objects
     return render(request, 'cafes/home.html', {'cafes':cafes})
 
-def detail(request, cafe_name):
-    print(cafe_name)
-    return render(request, 'cafes/home.html')
+def detail(request, cafe_id):
+    cafe_detail = get_object_or_404(Cafe, pk=cafe_id)
+    return render(request, 'cafes/details.html', {'cafe':cafe_detail})
